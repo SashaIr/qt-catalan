00156a88d6dbbff128b6003c054da9f534343854
Alessandro Iraci <SashaIr@users.noreply.github.com>
2025-12-11 18:45:20 +0100

    Update q_analog.lean

--- Project/q_analog.lean
+++ Project/q_analog.lean
@@ -18,12 +18,14 @@ def q_analog {R : Type*} [Semiring R] (n : ℕ) (q : R) : R :=
   | n + 1 => q_analog n q + q ^ n
 
 @[simp]
-theorem q_analog_eq_geom_sum {R : Type*} [DivisionRing R] (q : R) (n : ℕ) :
+theorem q_analog_eq_geom_sum {R : Type*} [Ring R] (n : ℕ) (q : R) :
     q_analog n q * (1 - q) = (1 - q ^ n) := by
   induction n with
   | zero => simp [q_analog]
   | succ n ih =>
-    rw [q_analog, add_mul, ih]; sorry
+    rw [q_analog, add_mul, ih]
+    simp only [mul_sub, mul_one, sub_add_sub_cancel, sub_right_inj]
+    exact Eq.symm (pow_succ q n)
 
 /-
 The q-analog of n when q=1 is equal to n.
@@ -60,25 +62,23 @@ theorem q_pochhammer_zero {R : Type*} [Ring R] (a q : R) : q_pochhammer 0 a q = 
 /-
 The q-binomial coefficient, denoted [n choose k]_q, is defined as [n]_q! / ([k]_q! * [n-k]_q!).
 -/
-def q_binomial {R : Type*} [DivisionRing R] (n k : ℕ) (q : R) : R :=
-  match k with
-  | 0     => 1
-  | k + 1 => match n with
-            | 0     => 0
-            | n + 1 => q_binomial n k q + q ^ (k + 1) * q_binomial n (k + 1) q
+def q_binomial {R : Type*} [Semiring R] : ℕ → ℕ → R → R
+  | _, 0, _     => 1
+  | 0, _, _     => 0
+  | n + 1, k + 1, q => q_binomial n k q + q ^ (k + 1) * q_binomial n (k + 1) q
 
 @[simp]
-theorem q_binomial_zero_right {R : Type*} [DivisionRing R] (n : ℕ) (q : R) : q_binomial n 0 q = 1 := by
+theorem q_binomial_zero_right {R : Type*} [Semiring R] (n : ℕ) (q : R) : q_binomial n 0 q = 1 := by
   simp [q_binomial]
 
 @[simp]
-theorem q_binomial_zero_left {R : Type*} [DivisionRing R] (k : ℕ) (q : R) : q_binomial 0 k q = (if k = 0 then 1 else 0) := by
+theorem q_binomial_zero_left {R : Type*} [Semiring R] (k : ℕ) (q : R) : q_binomial 0 k q = (if k = 0 then 1 else 0) := by
   cases k <;> simp [q_binomial]
 
 /-
 The q-Pascal identity states that [n+1 choose k+1]_q = [n choose k]_q + q^(k+1) * [n choose k+1]_q.
 -/
-theorem q_pascal {R : Type*} [DivisionRing R] (n k : ℕ) (q : R):
+theorem q_pascal {R : Type*} [Semiring R] (n k : ℕ) (q : R):
     q_binomial (n + 1) (k + 1) q = q_binomial n k q + q ^ (k + 1) * q_binomial n (k + 1) q := by
   rfl
 
@@ -89,7 +89,7 @@ def inv {n : ℕ} (S : Finset (Fin n)) : ℕ :=
 lemma inv_empty {n : ℕ} : inv (∅ : Finset (Fin n)) = 0 := by
   simp [inv]
 
-theorem sum_inv_eq_q_binomial {R : Type*} [Field R] (n k : ℕ) (q : R) (hq : q ≠ 0) :
+theorem sum_inv_eq_q_binomial {R : Type*} [Semiring R] (n k : ℕ) (q : R) (hq : q ≠ 0) :
     ∑ (S : Finset (Fin n)) with (fun S => S.card = k) S,
       q ^ inv S = q_binomial n k q := by
   induction' n with n ih generalizing k
